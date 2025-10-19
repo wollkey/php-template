de6f33b323d4446961a1f0908c21f99e42f69fc5
Alisher Tashmatov <pirojok167@gmail.com>
2025-10-19 17:52:17 +0300

    Configured php cs fixer

---
+++ .php-cs-fixer.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = new PhpCsFixer\Finder()->in(__DIR__);
+
+return new PhpCsFixer\Config()
+    ->setRules([
+        '@PSR12' => true,
+        '@Symfony' => true,
+        '@PHP8x4Migration' => true,
+    ])
+    ->setFinder($finder)
+    ->setCacheFile(__DIR__.'/var/.php-cs-fixer.cache')
+    ->setIndent("\t")
+    ->setLineEnding("\r\n");
