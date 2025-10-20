73d72af0dfa3c61d857c62330e2b90f93efc805b
Alisher Tashmatov <pirojok167@gmail.com>
2025-10-20 20:59:05 +0300

    CI workflow

--- rector.php
+++ rector.php
@@ -11,7 +11,7 @@ return RectorConfig::configure()
 		__DIR__.'/tests',
 	])
 	->withPhpSets(php84: true)
-    ->withImportNames()
+	->withImportNames()
 	->withTypeCoverageLevel(0)
 	->withDeadCodeLevel(0)
 	->withCodeQualityLevel(0);
